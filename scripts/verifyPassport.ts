64559bfd2a2b305881e8abddeb56dfe7ba8703a2
daveroga <daveroga@gmail.com>
2025-05-08 10:53:02 +0200

    update verify script for testing

--- scripts/verifyPassport.ts
+++ scripts/verifyPassport.ts
@@ -7,11 +7,23 @@ async function main() {
 
   const passportCredentialIssuer = await ethers.getContractAt(
     "PassportCredentialIssuer",
-    "0xe685CA0179c58C430E3d11532bB21BdFFD06261d",
+    "0xe7cCc474A89342f496E4dbbfCdBb942f627ffC9E", //"0xE3153601a25826D2C9d63B06CdD4c98AC70DB582",
   ); //"0xe685CA0179c58C430E3d11532bB21BdFFD06261d" //"0x03466F742d46F89a5125734e09e7934D3E11fbE5"); //"0xAbd91B9d85E83529699166933484687f7C7c8898");
   const imageHash = "0xc980e59163ce244bb4bb6211f48c7b46f88a4f40943e84eb99bdc41e129bd293";
-  await passportCredentialIssuer.addImageHashToWhitelist(imageHash);
-  await passportCredentialIssuer.addTransactor(await signer.getAddress());
+
+  const imageHashWhitelisted = await passportCredentialIssuer.isWhitelistedImageHash(imageHash);
+  const transactors = await passportCredentialIssuer.getTransactors();
+  console.log("Transactors:", transactors);
+  console.log("ImageHash is whitelisted:", imageHashWhitelisted);
+  
+  if (!imageHashWhitelisted) {
+    console.log("Adding imageHash ", imageHash);
+    await passportCredentialIssuer.addImageHashToWhitelist(imageHash);
+  }
+  if (!transactors.includes(await signer.getAddress())) {
+    console.log("Adding transactor ", await signer.getAddress());
+    await passportCredentialIssuer.addTransactor(await signer.getAddress());
+  }
   
   console.log("Transactors:", await passportCredentialIssuer.getTransactors());
   console.log("PassportCredentialIssuer version:", await passportCredentialIssuer.VERSION());
