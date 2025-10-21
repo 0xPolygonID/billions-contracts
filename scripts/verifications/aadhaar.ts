1fcd38fabae8c393a538f89e1f509408b4832e49
vbasiuk <volodymyr.basiuck@gmail.com>
2025-10-21 14:49:35 +0300

    add verifications scripts

---
+++ scripts/verifications/aadhaar.ts
@@ -0,0 +1,31 @@
+import { verifyContract } from "../../helpers/utils";
+import path from "path";
+import fs from "fs";
+import hre from "hardhat";
+import { contractsInfo } from "../../helpers/constants";
+
+async function main() {
+    const networkName = hre.network.config.chainId;
+    const deployedAddressesPath = path.join(
+        __dirname,
+        `../../ignition/deployments/chain-${networkName}/deployed_addresses.json`,
+      );
+      const deployedAddresses = JSON.parse(fs.readFileSync(deployedAddressesPath, "utf8"));
+    
+      const version = "V".concat(contractsInfo.PASSPORT_CREDENTIAL_ISSUER.version.replaceAll(".", "_").replaceAll("-", "_"));
+      const aadhaarCredentialIssuerAddress =
+        deployedAddresses[
+            `UpgradeAnonAadhaarCredentialIssuerModule${version}#AnonAadhaarCredentialIssuer`
+        ];
+  await verifyContract(aadhaarCredentialIssuerAddress, {
+    constructorArgsImplementation: [],
+    libraries: {},
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
